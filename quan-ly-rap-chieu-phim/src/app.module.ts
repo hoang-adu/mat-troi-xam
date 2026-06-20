c055896f8eb5309bf6602afa69042d057b9abb2f
anhlephuong <phuonganhhd6789@gmail.com>
2026-06-20 23:28:06 +0700

    Refactor app.module.ts to include new modules

--- quan-ly-rap-chieu-phim/src/app.module.ts
+++ quan-ly-rap-chieu-phim/src/app.module.ts
@@ -1,25 +1,53 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+ 
+// Thành viên 1
+import { MoviesModule }    from './movies/movies.module';
+import { RoomsModule }     from './rooms/rooms.module';
+import { SeatsModule }     from './seats/seats.module';
+import { CustomersModule } from './customers/customers.module';
+ 
+// Thành viên 2 
+import { EmployeesModule }  from './employees/employees.module';
+import { ShowtimesModule }  from './showtimes/showtimes.module';
+import { BookingsModule }   from './bookings/bookings.module';
+import { TicketsModule }    from './tickets/tickets.module';
+ 
+// Thành viên 3
+import { PaymentsModule }   from './payments/payments.module';
+import { ProductsModule }   from './products/products.module';
 import { FoodOrdersModule } from './food-orders/food-orders.module';
-
+ 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      autoLoadEntities: true,
-      synchronize: true,
+      host: 'mysql',            
+      port: 3306,
+      username: 'root',
+      password: 'root',         
+      database: 'rapchieuphim', 
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,      
     }),
+ 
+    // Thành viên 1
+    MoviesModule,
+    RoomsModule,
+    SeatsModule,
+    CustomersModule,
+ 
+    // Thành viên 2 
+    EmployeesModule,
+    ShowtimesModule,
+    BookingsModule,
+    TicketsModule,
+ 
+    // Thành viên 3
+    PaymentsModule,
+    ProductsModule,
     FoodOrdersModule,
   ],
   controllers: [AppController],
